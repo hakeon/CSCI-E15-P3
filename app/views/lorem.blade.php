640aedb1365a66ff1774b41417039c36e949e2fc
HAKEON <hakeon@gmail.com>
2014-11-06 21:17:03 -0500

    tweak css for submit buttons

--- app/views/lorem.blade.php
+++ app/views/lorem.blade.php
@@ -8,17 +8,16 @@
 @section('content')
 
 <h1>Lorem Ipsum</h1>
+<hr>
 
 	<div class="btn-group">
 		<h2>How many paragraphs do you need?</h2>
 		{{ Form::open(array('url' => '/loremtext', 'method' => 'POST')); }}
 		{{ Form::label('number', 'Please select a number from the drop-down:') }}
 		{{ Form::selectRange('number', 1,5) }}
-	</div>
-	<hr>
-	<div>
-		{{ Form::submit('Go!'); }}
+
+		{{ Form::submit('Go!') }}
     	{{ Form::close() }}
-	</div>
+    </div>
 
 @stop
--- app/views/user.blade.php
+++ app/views/user.blade.php
@@ -6,17 +6,17 @@
 @stop
 
 @section('content')
+
 <h1>Random User</h1>
+<hr>
 
 	<div class="btn-group">
 		<h2>How many users do you need?</h2>
 		{{ Form::open(array('url' => '/userlist', 'method' => 'POST')); }}
 		{{ Form::label('number', 'Please select a number from the drop-down:') }}
-		{{ Form::selectRange('number', 1, 100) }}
-	</div>
-	<hr>
-	<div>
-		{{ Form::submit('Go!'); }}
+		{{ Form::selectRange('number', 1, 99) }}
+
+		{{ Form::submit("Go!") }}
     	{{ Form::close() }}
 	</div>
 @stop
